cd85d36b49e8ff90070eb1c711225ad4f83b9c4f
Sheena Ou <so7122@mun.ca>
2019-07-19 18:04:20 -0230

    line closing complete

--- threshold.py
+++ threshold.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageFilter
-test_image = Image.open("color_floor_plan.jpg")
+import cv2
+import numpy as np
 
 def convert_to_grey_scale(image):
     """Converts an opened Image object to black/white"""
@@ -19,21 +20,31 @@ def erosion(image):
     filtered = image.filter(filter)
     return filtered
 
-test = convert_to_grey_scale(test_image)
-test1 = dilation(erosion(erosion(test)))
+def line_closing(image_file):
+    image = cv2.imread(image_file)
+    edges = cv2.Canny(image, 75, 150)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=160)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), (0, 0, 0), 5)
 
-save_image(test1, "test.jpg")
+    kernel = np.ones((5, 5), np.uint8)
+    dilate = cv2.dilate(image, kernel, iterations=2)
+    return dilate
 
-import cv2
-import numpy as np
+def opencv_write(image, name):
+    cv2.imwrite(name, image)
 
-img = cv2.imread("test.jpg")
-edges = cv2.Canny(img, 75, 150)
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50,maxLineGap=250)
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
-# Show result
-cv2.imwrite("edges.png", edges)
+def main():
+    test_image = Image.open("color_floor_plan.jpg")
+    grey = convert_to_grey_scale(test_image)
+    threshold = dilation(erosion(erosion(grey)))
+    save_image(threshold, "temp.png")
+    new_image = line_closing("temp.png")
+    opencv_write(new_image, "new_image.jpg")
 
-cv2.imwrite("Result.jpg", img)
+main()
+
+
+
+
